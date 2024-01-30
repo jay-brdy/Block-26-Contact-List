5156d3bcc403d9e453d7ab10fc5e652f5911f118
jay-brdy <jaysirit@gmail.com>
2024-01-29 21:42:26 -0600

    fetching data and useEffect

--- src/components/ContactList.jsx
+++ src/components/ContactList.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { useState } from "react"
 import ContactRow from "./ContactRow"
+import { useEffect } from "react";
 
 const dummyContacts = [
     { id: 1, name: "R2-D2", phone: "222-222-2222", email: "r2d2@droids.com" },
@@ -11,23 +12,39 @@ const dummyContacts = [
 export default function ContactList() {
     const [contacts, setContacts] = useState(dummyContacts);
 
-    return (
-        <table>
-            <thead>
-                <tr>
-                    <th colSpan="3">Contact List</th>
-                </tr>
-            </thead>
-            <tbody>
-                <tr>
-                    <td>Name</td>
-                    <td>Email</td>
-                    <td>Phone</td>
-                </tr>
-                {contacts.map((contact) => {
-                    return <ContactRow key={contact.id} contact={contact} />;
-                })}
-            </tbody>
-        </table>
-    );
+    useEffect(() => {
+        async function fetchContacts() {
+          try {
+            // fetch logic goes here
+            const response = await fetch(
+                "https://fsa-jsonplaceholder-69b5c48f1259.herokuapp.com/users"
+            );
+            const result = await response.json();
+            setContacts(result);
+          } catch (error) {
+            console.error(error);
+          }
+        }
+        fetchContacts();
+      }, []);
+
+return (
+    <table>
+        <thead>
+            <tr>
+                <th colSpan="3">Contact List</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <td>Name</td>
+                <td>Email</td>
+                <td>Phone</td>
+            </tr>
+            {contacts.map((contact) => {
+                return <ContactRow key={contact.id} contact={contact} />;
+            })}
+        </tbody>
+    </table>
+);
 }
