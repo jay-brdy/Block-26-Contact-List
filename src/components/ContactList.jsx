e0d1d58748033487b2e927ffd3acddd431cb7a42
jay-brdy <jaysirit@gmail.com>
2024-01-31 02:51:42 -0600

    adding more functionality

--- src/components/ContactList.jsx
+++ src/components/ContactList.jsx
@@ -9,42 +9,42 @@ const dummyContacts = [
     { id: 3, name: "BB-8", phone: "888-888-8888", email: "bb8@droids.com" },
 ];
 
-export default function ContactList() {
+export default function ContactList({ setSelectedContactId }) {
     const [contacts, setContacts] = useState(dummyContacts);
 
     useEffect(() => {
         async function fetchContacts() {
-          try {
-            // fetch logic goes here
-            const response = await fetch(
-                "https://fsa-jsonplaceholder-69b5c48f1259.herokuapp.com/users"
-            );
-            const result = await response.json();
-            setContacts(result);
-          } catch (error) {
-            console.error(error);
-          }
+            try {
+                // fetch logic goes here
+                const response = await fetch(
+                    "https://fsa-jsonplaceholder-69b5c48f1259.herokuapp.com/users"
+                );
+                const result = await response.json();
+                setContacts(result);
+            } catch (error) {
+                console.error(error);
+            }
         }
         fetchContacts();
-      }, []);
+    }, []);
 
-return (
-    <table>
-        <thead>
-            <tr>
-                <th colSpan="3">Contact List</th>
-            </tr>
-        </thead>
-        <tbody>
-            <tr>
-                <td>Name</td>
-                <td>Email</td>
-                <td>Phone</td>
-            </tr>
-            {contacts.map((contact) => {
-                return <ContactRow key={contact.id} contact={contact} />;
-            })}
-        </tbody>
-    </table>
-);
+    return (
+        <table>
+            <thead>
+                <tr>
+                    <th colSpan="3">Contact List</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>Name</td>
+                    <td>Email</td>
+                    <td>Phone</td>
+                </tr>
+                {contacts.map((contact) => {
+                    return <ContactRow key={contact.id} contact={contact} />;
+                })}
+            </tbody>
+        </table>
+    );
 }
