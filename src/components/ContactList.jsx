15c6ccc98ebdccf4d01867916ad3344f76f98afc
jay-brdy <jaysirit@gmail.com>
2024-02-01 22:31:01 -0600

    selected a user now works

--- src/components/ContactList.jsx
+++ src/components/ContactList.jsx
@@ -44,7 +44,7 @@ export default function ContactList({ setSelectedContactId }) {
                     <td>Phone</td>
                 </tr>
                 {contacts.map((contact) => {
-                    return <ContactRow key={contact.id} contact={contact} />;
+                    return <ContactRow key={contact.id} contact={contact} setSelectedContactId={setSelectedContactId} />;
                 })}
             </tbody>
         </table>
